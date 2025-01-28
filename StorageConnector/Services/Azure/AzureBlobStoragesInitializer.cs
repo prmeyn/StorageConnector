1c0241b46d7c10b5d68e08be3c632351268ee997
Prashansa Meyn <pm@globalpay.dk>
2025-01-28 23:11:14 +0100

    connect

---
+++ StorageConnector/Services/Azure/AzureBlobStoragesInitializer.cs
@@ -0,0 +1,40 @@
+﻿using EarthCountriesInfo;
+using Microsoft.Extensions.Configuration;
+
+namespace StorageConnector.Services.Azure
+{
+	public sealed class AzureBlobStoragesInitializer
+    {
+		internal readonly AzureBlobStorageSettings AzureBlobStorageSettings;
+
+		public AzureBlobStoragesInitializer(IConfiguration configuration)
+        {
+            var azureConfig = configuration.GetSection("StorageConnectors:Azure");
+			AzureBlobStorageSettings = new AzureBlobStorageSettings
+			{
+				CountryIsoCodeMapToAccountName = ParseCountryIsoCodeMap(azureConfig.GetSection("CountryIsoCodeMapToAccountName").Get<Dictionary<string, string>>()),
+				Accounts = azureConfig.GetRequiredSection("Accounts").Get<List<AzureAccount>>()
+			};
+		}
+
+		private Dictionary<CountryIsoCode, string> ParseCountryIsoCodeMap(Dictionary<string, string>? rawMap)
+		{
+			var parsedMap = new Dictionary<CountryIsoCode, string>();
+
+			foreach (var kvp in rawMap)
+			{
+				if (Enum.TryParse(kvp.Key, ignoreCase: true, out CountryIsoCode countryIsoCode))
+				{
+					parsedMap[countryIsoCode] = kvp.Value;
+				}
+				else
+				{
+					// Handle invalid country codes (e.g., log a warning or throw an exception)
+					throw new ArgumentException($"Invalid country ISO code: {kvp.Key}");
+				}
+			}
+
+			return parsedMap;
+		}
+	}
+}
--- StorageConnector/SeviceCollectionExtensions.cs
+++ StorageConnector/SeviceCollectionExtensions.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using StorageConnector.Services.Azure;
 
 namespace StorageConnector
 {
-	public static class SeviceCollectionExtensions
+    public static class SeviceCollectionExtensions
 	{
 		public static void AddStorageConnectors(this IServiceCollection services)
 		{
-			
+			services.AddSingleton<AzureBlobStoragesInitializer>();
 		}
 	}
 }
